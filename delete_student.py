20a93063962fe1548d0789856ae137e88cc00556
NielPatelSE12 <NielPatel1212@outlook.com>
2024-07-22 17:04:10 -0400

    added comments for understanding

--- delete_student.py
+++ delete_student.py
@@ -2,14 +2,15 @@ import create_new_student
 
 class DeleteStudent:
     def __init__(self):
+        #creating the instance 
         self.delete_student_by_id()
-
+    # Getting the student id to be able to delete the student profile
     def delete_student_by_id(self):
         student_id_to_delete = input("Enter the student ID to delete:")
-        
+        #Opening the file
         with open("student.csv","r") as f:
             data = f.readlines()
-
+    #Read the line to find the student id that matches what the user has provided.
         with open("student.csv","w",newline="") as f:
             for line in data:
                 if line.strip() and not line.startswith(student_id_to_delete):
--- view_student.py
+++ view_student.py
@@ -2,8 +2,9 @@ import create_new_student
 
 class ViewStudentProfile:
     def __init__(self):
+        #opening student.csv file for read mode
         self.studentFile = open("student.csv", "r+")
-
+    #function for displaying students in the file
     def view_student_profile(self):
         print("Students:")
         print(self.studentFile.read())
